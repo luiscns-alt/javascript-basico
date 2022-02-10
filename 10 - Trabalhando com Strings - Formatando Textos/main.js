d553fbf38685b1b2cdcc3a7a3060d45309ae70fe
Luis Carlos <luiscarlosnors@gmail.com>
2022-02-09 23:29:00 -0300

    trabalhando com strings - propriedades e métodos

---
+++ 10 - Trabalhando com Strings - Formatando Textos/main.js
@@ -0,0 +1,11 @@
+"hello World".lastIndexOf("world");
+"hello World".substr(0, 5);
+"hello World".substr(1, 5);
+"hello World".substr(2, 5);
+"hello World".toUpperCase();
+"hello World".toLowerCase();
+"hello World".trimLeft();
+"hello World".trimRight();
+"hello World".trim();
+"hello World".split(" ");
+"h.e.l.l.o. W.o.r.l.d.".split(".");
